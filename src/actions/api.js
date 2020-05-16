cfbfade8c84decd220db2548e5588eb2dc47b8c4
ishantsolanki <ishant360@gmail.com>
2020-05-16 22:08:26 +0100

    Make search specific to restaurants

--- src/actions/api.js
+++ src/actions/api.js
@@ -1,5 +1,5 @@
-export const getSuggestedVenues = (clientId, clientSecret, query) => fetch(`https://api.foursquare.com/v2/venues/search?near=London&query=${query}&client_id=${clientId}&client_secret=${clientSecret}&v=20200226`)
-export const getSimilarVenues = (clientId, clientSecret, venueId) => fetch(`https://api.foursquare.com/v2/venues/${venueId}/similar?client_id=${clientId}&client_secret=${clientSecret}&v=20200226`)
+export const getSuggestedVenues = (clientId, clientSecret, query) => fetch(`https://api.foursquare.com/v2/venues/search?near=London&query=${query}&client_id=${clientId}&client_secret=${clientSecret}&v=20200226&categoryId=4d4b7105d754a06374d81259`)
+export const getSimilarVenues = (clientId, clientSecret, venueId) => fetch(`https://api.foursquare.com/v2/venues/${venueId}/similar?client_id=${clientId}&client_secret=${clientSecret}&v=20200226&categories=4d4b7105d754a06374d81259`)
 
 export default {
   getSuggestedVenues,
