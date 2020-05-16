f245ad7198dc67366e3ddf79b8e1acb258c4ddb9
ishantsolanki <ishant360@gmail.com>
2020-05-16 10:39:37 +0100

    Fix adding the nodes instead of appending when data changes

--- src/components/graphs/SimilarRestaurantsGraph.jsx
+++ src/components/graphs/SimilarRestaurantsGraph.jsx
@@ -34,34 +34,32 @@ export const SimilarRestaurantsGraph = ({ graphMounted, dataNodes, dataLinks }) 
         .force("center", forceCenter(250, 250));
 
       const link = select(d3Ref.current)
-        .append("g")
-        .attr("stroke", "#999")
-        .attr("stroke-opacity", 0.6)
         .selectAll("line")
         .data(links)
         .join("line")
+        .attr("stroke", "#999")
+        .attr("stroke-opacity", 0.6)
         .attr("stroke-width", d => Math.sqrt(d.value));
 
       const node = select(d3Ref.current)
-        .append("g")
-        .attr("stroke", "#fff")
-        .attr("stroke-width", 1.5)
         .selectAll("circle")
         .data(nodes)
         .join("circle")
         .attr("r", 5)
+        .attr("stroke", "#fff")
+        .attr("stroke-width", 1.5)
         .attr("fill", 'black');
 
       simulation.on("tick", () => {
         link
-            .attr("x1", d => d.source.x)
-            .attr("y1", d => d.source.y)
-            .attr("x2", d => d.target.x)
-            .attr("y2", d => d.target.y);
+          .attr("x1", d => d.source.x)
+          .attr("y1", d => d.source.y)
+          .attr("x2", d => d.target.x)
+          .attr("y2", d => d.target.y);
 
         node
-            .attr("cx", d => d.x)
-            .attr("cy", d => d.y);
+          .attr("cx", d => d.x)
+          .attr("cy", d => d.y);
       });
     }
   }, [dataLinks, dataNodes, graphMounted]);
