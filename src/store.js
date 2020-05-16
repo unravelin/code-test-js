afc5fb0cbaef5b92036aba466a9bf8df37704f69
ishantsolanki <ishant360@gmail.com>
2020-05-16 14:49:42 +0100

    Polyfill to debug es5 maps in redux devtools

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware, compose } from 'redux';
 import reducers from './reducers';
+import 'map.prototype.tojson';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
